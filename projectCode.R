631cd39c401687c89d3edc42e793df539d829ad1
seanlydon20 <sdl5402@psu.edu>
2025-05-01 19:26:52 -0400

    Updated Code

--- projectCode.R
+++ projectCode.R
@@ -6,32 +6,73 @@ library(dplyr)
 library(lubridate)
 library(kableExtra)
 library(ggplot2)
+
 majorCity_globalTemp <- GlobalLandTemperaturesByCity %>%
-  filter(City == c("New York", "San Fransisco", "Los Angeles", "Chicago",
+  filter(City == c("New York", "San Francisco", "Los Angeles", "Chicago",
                    "Houston","Philadelphia")) %>%
   filter(Country == "United States") %>%
   filter(dt >= as.Date("1913-01-01"))
 
 
 yearly_avg_temp <- majorCity_globalTemp %>%
-  mutate(Year = year(dt)) %>%  # Extract the year from the date
+  mutate(Year = year(dt), 
+         AverageTemperatureF = (AverageTemperature * 9/5) + 32) %>%# Extract the year from the date
   group_by(City, Year) %>%
-  summarize(AvgTemperature = mean(AverageTemperature, na.rm = TRUE)) # Calculate the yearly average
+  summarize(AvgTemperatureF = mean(AverageTemperatureF, na.rm = TRUE)) # Calculate the yearly average
 
+
+avg_NY <- yearly_avg_temp %>%
+  filter(City == "New York")
+
+avg_SF <- yearly_avg_temp %>%
+  filter(City == "San Francisco")
+
+avg_LA <- yearly_avg_temp %>%
+  filter(City == "Los Angeles")
+
+avg_Hous <- yearly_avg_temp %>%
+  filter(City == "Houston")
+
+avg_Phil <- yearly_avg_temp %>%
+  filter(City == "Philadelphia")
+
+avg_Chi <- yearly_avg_temp %>%
+  filter(City == "Chicago")
+
+avg_Cali <- yearly_avg_temp %>%
+  filter(City %in% c("Los Angeles", "San Francisco"))
 
 # Create a plot for each city
-city_plots <- map(unique(yearly_avg_temp$City), function(city_name) {
-  ggplot(yearly_avg_temp %>% filter(City == city_name), aes(x = Year, y = AvgTemperature)) +
-    geom_line() +
-    geom_point(size = 0.5, alpha = 0.7) +
-    labs(
-      title = paste("Temperature Trend for", city_name),
-      x = "Year",
-      y = "Average Temperature (°C)"
-    ) +
-    scale_x_continuous(breaks = seq(1910, 2010, 10)) +
-    theme_minimal() +
-    theme(plot.title = element_text(hjust = 0.5))
-  })
+ggplot(avg_Cali, 
+       aes(
+         x = Year, 
+         y = AvgTemperatureF, 
+         color = City,
+         linetype = City)) +
+  geom_smooth(se = FALSE, size = 1) +
+  theme_minimal() +
+  labs(title = "Average Temperature of 2 California Cities over 100 Years",
+       x = "Year",
+       y = "Average Temperature (F)") +
+  theme(legend.title = element_blank())
+
+ggplot(avg_Phil, 
+       aes(
+         x = Year, 
+         y = AvgTemperatureF, 
+         color = City,
+         linetype = City)) +
+  geom_smooth(se = FALSE, size = 1) +
+  theme_minimal() +
+  labs(title = "Average Temperature of Philadelphia over 100 Years",
+       x = "Year",
+       y = "Average Temperature (F)") +
+  theme(legend.title = element_blank())
+
+
+
+
+
+
 
 
